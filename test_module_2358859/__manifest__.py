6803094495addae2c3d9621150f7a8f4d838c7b7
Nicolas Galler <ngl@odoo.com>
2020-10-19 15:08:26 +0200

    Add test module

---
+++ test_module_2358859/__manifest__.py
@@ -0,0 +1,24 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Test Module 2358859',
+    'author': '',
+    'maintainer': '',
+    'license': '',
+    'description': """Test.
+        """,
+    'summary': """Test.""",
+    'category': '',
+    'website': '',
+    'version': '1.0',
+    'depends': [
+        'sale_timesheet',
+        'calendar',
+        'website',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
